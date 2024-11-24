4f17a134b9e6f97b8f14ad7fab67dc6cfcf96233
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-11-24 22:16:23 +0100

    Fixed the issue, back to basics

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.Maui.Controls;
+﻿using MauiApp1.Models;
+using MauiApp1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 
 namespace MauiApp1
 {
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : ContentPage
     {
-        public MainPage()
+        private readonly DatabaseService _databaseService;
+
+        public MainPage(DatabaseService databaseService)
         {
             InitializeComponent();
+            _databaseService = databaseService;
+            LoadCustomersAsync();
+        }
+
+        private async void LoadCustomersAsync()
+        {
+            var customers = await _databaseService.GetCustomersAsync();
+            CustomersCollectionView.ItemsSource = customers;
         }
     }
 }
