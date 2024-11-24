75a46030db52e78c2e7a03bc82c09e854d44ce7a
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-11-24 23:40:04 +0100

    Implement customer list display with CollectionView and data loading

--- MauiApp1/Views/CustomersPage.xaml.cs
+++ MauiApp1/Views/CustomersPage.xaml.cs
@@ -1,12 +1,24 @@
+using MauiApp1.Models;
+using MauiApp1.Services;
 using Microsoft.Maui.Controls;
 
 namespace MauiApp1
 {
     public partial class CustomersPage : ContentPage
     {
-        public CustomersPage()
+        private readonly DatabaseService _databaseService;
+
+        public CustomersPage(DatabaseService databaseService)
         {
             InitializeComponent();
+            _databaseService = databaseService;
+            LoadCustomersAsync();
+        }
+
+        private async void LoadCustomersAsync()
+        {
+            var customers = await _databaseService.GetCustomersAsync();
+            CustomersCollectionView.ItemsSource = customers;
         }
     }
 }
