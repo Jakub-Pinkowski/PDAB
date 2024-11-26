8b3f9c1ed9763470b66e7798cb63e4ce4de4e381
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-11-26 15:23:52 +0100

    Initialize ProductName property with an empty string in OrderDetail model

--- MauiApp1/Models/OrderDetail.cs
+++ MauiApp1/Models/OrderDetail.cs
@@ -4,7 +4,7 @@ namespace MauiApp1.Models
     {
         public int OrderId { get; set; }
         public DateTime OrderDate { get; set; }
-        public string ProductName { get; set; }
+        public string ProductName { get; set; } = string.Empty;
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public decimal TotalAmount { get; set; }
